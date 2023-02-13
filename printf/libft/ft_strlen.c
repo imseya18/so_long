332e5575ef5f159c5b0f1e6497696cff18da6e31
Mathieu Morue <mmorue@k0r2p8.42mulhouse.fr>
2023-02-13 17:28:06 +0100

    strlen

--- printf/libft/ft_strlen.c
+++ printf/libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: mmorue <mmorue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 12:39:59 by mmorue            #+#    #+#             */
-/*   Updated: 2023/01/13 16:02:07 by mmorue           ###   ########.fr       */
+/*   Updated: 2023/02/13 16:01:45 by mmorue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strlen(const char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0' && str[i] != '\n')
+	while (str[i] != '\0')
 		i++;
 	return (i);
 }
